843e26819d237a5738ccc38899201c99e2240f59
evanzha <evanzha@tencent.com>
2023-11-02 22:32:34 +0800

    feat: add

--- src/class02_sort2/Code01_MergeSort.java
+++ src/class02_sort2/Code01_MergeSort.java
@@ -50,4 +50,13 @@ public class Code01_MergeSort {
             arr[l+i] = help[i];
         }
     }
+
+    public static void main(String[] args) {
+        int arr[] = {5,7,4,6,1,3,2,9};
+        mergeSort(arr);
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
 }
