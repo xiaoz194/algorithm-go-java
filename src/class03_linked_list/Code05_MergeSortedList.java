78fd3323af8e39afe1dacbc3d5dd485b75057909
xiaoz194 <2043439620@qq.com>
2024-11-24 19:37:45 +0800

    feat: 合并有序链表，链表相加

---
+++ src/class03_linked_list/Code05_MergeSortedList.java
@@ -0,0 +1,45 @@
+package class03_linked_list;
+
+// 合并两条有序链表
+public class Code05_MergeSortedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null || list2 == null) {
+            return list1 == null ? list2 : list1;
+        }
+        ListNode head = list1.val <= list2.val ? list1 : list2;
+        ListNode c1 = head.next;
+        ListNode c2 = head == list1 ? list2 : list1;
+        ListNode pre = head;
+        while (c1 != null && c2 != null) {
+            if (c1.val <= c2.val) {
+                pre.next = c1;
+                c1 = c1.next;
+            } else {
+                pre.next = c2;
+                c2 = c2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = c1 == null ? c2 : c1;
+        return head;
+    }
+
+}
---
+++ src/class03_linked_list/Code06_addTwoNumbers.java
@@ -0,0 +1,45 @@
+package class03_linked_list;
+
+// 两数相加 链表相加
+public class Code06_addTwoNumbers {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int sum = (v1 + v2 + carry) % 10;
+            carry = (v1 + v2 + carry) / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+            cur = cur.next;
+        }
+        return pre.next;
+    }
+}
